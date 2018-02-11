1739fb03792840604fb22e3659631ffca3b1c874
jessica van rooyen <jessicav556@gmail.com>
2018-02-11 18:31:38 +0200

    📁organizing resolvers and resolutions folders

---
+++ apollo/imports/api/resolutions/resolvers.js
@@ -0,0 +1,16 @@
+export default {
+    Query: {
+        resolutions() {
+            return [
+                {
+                    _id: "words",
+                    name: "Get stuff done!"
+                },
+                {
+                    _id: "more words",
+                    name: "Lose Some Weight!"
+                }
+            ];
+        }
+    }
+};
