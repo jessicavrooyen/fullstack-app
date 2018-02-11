d8f8e23b2de85e062f1b45590d4efca836516977
jessica van rooyen <jessicav556@gmail.com>
2018-02-11 19:07:38 +0200

    💪🏻  added a new type: mutations

--- apollo/imports/api/resolutions/resolvers.js
+++ apollo/imports/api/resolutions/resolvers.js
@@ -1,13 +1,20 @@
 import Resolutions from './resolutions';
 
-// Resolutions.insert({
-//     name: 'Test Res',
-// });
+
 
 export default {
     Query: {
         resolutions() {
             return Resolutions.find({}).fetch();
+        }
+    },
+
+    Mutation: {
+        createResolution() {
+            console.log('got here');
+            // const resolutionId = Resolutions.insert({
+            //     name: 'Test Res'
+            // });
         }
     }
 };
